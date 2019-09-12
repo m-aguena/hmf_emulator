d96e6797b93a5305f674f49471a5805dc076d8ae
m-aguena <michel.aguena@gmail.com>
2019-09-12 14:10:21 -0300

    update cosmosis/module.py to cosmosis standards

--- cosmosis/module.py
+++ cosmosis/module.py
@@ -7,6 +7,8 @@ sys.path.append('/'.join(os.path.realpath(__file__).split('/')[:-2]))
 print sys.path
 import hmf
 
+mf_block_name = "mass_function_aem"
+
 def setup(options):
 
     # We use a class as a namespace here to return config information
@@ -14,7 +16,11 @@ def setup(options):
     class config:
 
         # Name of the section to get matter power from
-        sigma_dir  = options.get_string(option_section, "sigma")
+        sigma_block  = options.get_string(option_section, "sigma")
+        default_tinker = options.get_bool(option_section, "tinker_parameters", False)
+
+        # printout settings
+        verbose = int(options.get_bool(option_section, "verbose", False))
 
     return config
 
@@ -41,63 +47,56 @@ def execute(block, config):
         Neff is HADRCODED with fixed value of 3.0!
     '''
 
-    '''
-    cosm_dic = {
-        'H0':'h0',
-        'As':'A_s',
-        'n_s':'n_s',
-        'w0':'w',
-        'omega_b':'omega_b',
-        'omega_m':'omega_m',
-        #'N_eff':'massless_nu',
-            }
-    for n, p in cosm_dic.items():
-        cosm[n] = block[names.cosmological_parameters, p]
-    '''
-    cosm = {n:block[names.cosmological_parameters, n]
-        for n in ('h0','A_s','n_s','w','omega_b','omega_m',)}
-
-    cosm['N_eff'] = 3.0
-    cosm['w0'] = cosm['w']
-    cosm['omega_cdm'] = (cosm['omega_m']-cosm['omega_b'])*cosm['h0']**2
-    cosm['omega_b'] = cosm['omega_b']*cosm['h0']**2
-    cosm['H0'] = 100.*cosm['h0']
-    cosm['ln10As'] = np.log10(cosm['A_s']*1e10)
-
-#    "omega_b": (OmB)*h**2,
-#    "omega_cdm": (OmM-OmB)*h**2,
-
-    print('cosm:',cosm)
-        
-    # Just a simple rename for clarity.
-    sigma_dir = config.sigma_dir
-
-    # Load sigma(M) from the block
-    m = block[sigma_dir, 'm']
-    z = block[sigma_dir, 'z']
-
-    print m
-    # make sigma(M) function for emulator
-    sig2 = block[sigma_dir, 'sigma2']#.T
-    print sig2.shape
-    sig2_interp = RectBivariateSpline(m*ln10, z, sig2)
+    # Initalize aemulator
+    aem = hmf.hmf_emulator(use_class=False)
 
 
-    # first approximation of dsig2/dM, will have to come from sigma(M) in the future
-    mm = .5*(m[:-1]+m[1:])
-    dsig2 = np.array([(y/x)/(ln10*10**xm) for y, x, xm 
-            in zip((sig2[1:]-sig2[:-1]), (m[1:]-m[:-1]), mm)])
-    dsig2_interp = RectBivariateSpline(mm*ln10, z, dsig2)
-
-    use_class, sigma_funcs = False, (sig2_interp, dsig2_interp)
-    #use_class, sigma_funcs = True, None
-
-    aem = hmf.hmf_emulator(use_class=use_class, default_tinker=True)
+    # Cosmological parameters
+    h0 = block[names.cosmological_parameters,'h0']
+    A_s = block[names.cosmological_parameters,'A_s']
+    n_s = block[names.cosmological_parameters,'n_s']
+    w = block[names.cosmological_parameters,'w']
+    omega_b = block[names.cosmological_parameters,'omega_b']
+    omega_m = block[names.cosmological_parameters,'omega_m']
+    
+    # dictionary for aemulator
+    cosm = {
+        'N_eff': 3.0,
+        'w0': w,
+        'omega_cdm': (omega_m-omega_b)*h0**2,
+        'omega_b': omega_b*h0**2,
+        'H0': 100.*h0,
+        'ln10As': np.log10(A_s*1e10),
+        'n_s': n_s,
+    }
+    
+    if config.verbose:
+        print('cosm:',cosm)
+    
+    # Pass cosmology to aemulator
     aem.set_cosmology(cosm)
-    dndlnM = aem.dndM(10**m, z, sigma_funcs)*(10**m)
 
-    print dndlnM.shape
-    block["mf", "mf" ] = dndlnM
+    # Load logm, z, sigma(M) and dsigma2 from the block
+    sig_r, sig_z, sigma2 = block.get_grid(config.sigma_block, "R", "z", "sigma2")
+    sig_logm = block[config.sigma_block, 'logm']
+    sig_m = 10**sig_logm
+    dsigma2dlnm = block[config.sigma_block, 'dsigma2dlnM']
+    dsigma2dm = np.array([ds/mv for ds, mv in zip(dsigma2dlnm, sig_m)])
+    
+    # make splines of sigma(M) and dsigma functions for emulator
+    sig2_interp = RectBivariateSpline(sig_logm*ln10, sig_z, sigma2)
+    dsig2_interp = RectBivariateSpline(sig_logm*ln10, sig_z, dsigma2dm)
+
+    # Get dndlnM from aemulator
+    m, z = sig_m, sig_z
+    dndlnM = aem.dndM(m, z, sigma_funcs=(sig2_interp, dsig2_interp),
+                default_tinker=config.default_tinker)*m
+
+    if config.verbose:
+        print dndlnM.shape
+
+    # We save the grid dndlnM(logM,z)
+    block.put_grid(mf_block_name, "z", z, "logm", np.log10(m), "dndlnm", dndlnM)
 
     #We tell CosmoSIS that everything went fine by returning zero
     return 0
